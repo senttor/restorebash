b546ba1365ef2414c9959184e287d6020711a054
viktor <oksentiuk.viktor@gmail.com>
2019-02-19 16:29:04 +0200

    oops

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ path="$HOME/backend"
 pathstore="$HOME/backend/web/uploads"
 var="$1.zip"
 vardate=$1
-if [[ $vardate =~ ^[0-9]{2}-[0-9]{2}-[00-9]{4}$ ]];
+if [[ $vardate =~ ^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]];
 then
 	unzip  $path/$var -d $pathstore
 

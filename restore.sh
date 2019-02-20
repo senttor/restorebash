e524e932bc32672b978e19389d91cfa75e13ec79
viktor <oksentiuk.viktor@gmail.com>
2019-02-20 18:43:21 +0200

    test rsa

--- restore.sh
+++ restore.sh
@@ -3,7 +3,7 @@ set -e
 BACKUPS_ROOT="backups/"$1
 BACKEND_ROOT="backend/web/uploads/"
 vardate=$1
-if [[ $vardate =~ ^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]];
+if [[ $vardate =~ ^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]];# if [[ -z $1 ]]
 then
 	gunzip -r $BACKUPS_ROOT
 	mv  $BACKUPS_ROOT/*  $BACKEND_ROOT
